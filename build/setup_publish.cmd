9520d35b517c1b2e9bb9eab4e81d8726dfeb4c7d
k.12f <happyaifaruxue@gmail.com>
2024-09-24 11:05:50 +0800

    feat: 修复build 打包问题 & 添加安装有卸载猫猫图

--- build/setup_publish.cmd
+++ build/setup_publish.cmd
@@ -1,30 +1,40 @@
 cd /d %~dp0
 set app_dir=../src/BlueCatKoKo.Ui
 set app_bin_dir=..\src\BlueCatKoKo.Ui\bin\Release\net8.0-windows\publish
-set app_name=BlueCatKoKo
+set app_name=BlueCatKoKo.Ui
+set setup_name=BlueCatKoKo
+set tempfolder=%~dp0dist
+
+mkdir %tempfolder%\%app_name%
 
 @echo [prepare somethings]
-del MicaSetup.exe
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath`) do set "path=%path%;%%i\MSBuild\Current\Bin;%%i\Common7\IDE"
 
 @echo [prepare version]
 cd /d %app_dir%
-set "script=Get-Content 'BlueCatKoKo.Ui.csproj' | Select-String -Pattern 'AssemblyVersion\>(.*)\<\/AssemblyVersion' | ForEach-Object { $_.Matches.Groups[1].Value }"
+set "script=Get-Content "%app_name%.csproj" | Select-String -Pattern 'AssemblyVersion\>(.*)\<\/AssemblyVersion' | ForEach-Object { $_.Matches.Groups[1].Value }"
 for /f "usebackq delims=" %%i in (`powershell -NoLogo -NoProfile -Command "%script%"`) do set version=%%i
 echo current version is %version%
 if "%b%"=="" ( set "b=%version%" )
 
 echo [build app using vs2022]
-cd ..\src\
+cd /d %~dp0
+rd /s /q %app_bin_dir%
+cd /d %app_dir%
 dotnet restore
 dotnet publish -c Release -p:PublishProfile=FolderProfile
-cd /d %~dp0
 
 echo [pack app using 7z]
-del %app_dir%
-MicaSetup.Tools\7-Zip\7z a publish.7z %app_dir%\* -t7z -mx=5 -mf=BCJ2 -r -y
+cd /d %~dp0
+rd /s /q %tempfolder%
+cd /d %app_bin_dir%
+xcopy * "%tempfolder%\%app_name%" /E /C /I /Y
+
+cd /d %~dp0
+MicaSetup.Tools\7-Zip\7z a publish.7z %tempfolder%\%app_name%\* -t7z -mx=5 -mf=BCJ2 -r -y
 copy /y publish.7z .\MicaSetup\Resources\Setups\publish.7z
-move publish.7z BlueCatKoKo_v%version%.7z
+move publish.7z %setup_name%_v%version%.7z
+rd /s /q %tempfolder%
 
 @echo [build uninst using vs2022]
 msbuild MicaSetup\MicaSetup.Uninst.csproj /t:Rebuild /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=FolderProfile /restore
@@ -34,8 +44,10 @@ copy /y .\MicaSetup\bin\Release\net472\MicaSetup.exe .\MicaSetup\Resources\Setup
 msbuild MicaSetup\MicaSetup.csproj /t:Build /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=FolderProfile /restore
 
 @echo [finish]
+cd /d %~dp0
+dir
 del /f /q MicaSetup.exe
 copy /y .\MicaSetup\bin\Release\net472\MicaSetup.exe .\
-move MicaSetup.exe BlueCatKoKoSetup_v%version%.exe
+move MicaSetup.exe %setup_name%_Setup_v%version%.exe
 
 @pause
---
+++ build/test.cmd
@@ -0,0 +1,9 @@
+﻿set app_dir=../src/BlueCatKoKo.Ui
+set app_bin_dir=..\src\BlueCatKoKo.Ui\bin\Release\net8.0-windows\publish
+set app_name=BlueCatKoKo
+set tempfolder=%~dp0dist\BlueCatKoKo
+
+cd /d %~dp0
+rd /s /q %tempfolder%
+
+@pause
